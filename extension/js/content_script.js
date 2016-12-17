5588101ce1fe99f4a52c973ca347b3724c541b53
Jon White <jjwhite01@gmail.com>
2016-12-16 23:31:05 -0500

    got it working but doesn't work with shortened urls

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -1,6 +1,25 @@
 $(function() {
     $('a').click(function(e){
-		e.preventDefault();
-		chrome.windows.create({"url": this.href, "incognito": true});
+		console.log(this.href);
+		if (check(this.href)){
+			chrome.runtime.sendMessage({'href':this.href},function(response){})
+			e.preventDefault();
+		}
+		
 	});
 });
+
+var check = function(href){
+		var result = urls.some(function(elm){
+			if(href.indexOf(elm) !== -1)
+				return true;
+		});
+    
+    return result;
+
+}
+
+var urls = [
+"nytimes.com",
+"washingtonpost.com"
+];
